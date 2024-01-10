6e91a63b3d705e31aeb6960a93b8467a235efa61
Andres Norato <carnorat@gmail.com>
2024-01-10 12:05:05 -0500

    created and delete witch signals

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,9 +14,16 @@ tasks = signal([
 ]);
 
 
+changeHamdler(event: Event){
+  const input = event.target as HTMLInputElement;
+  const newTaks = input.value;
+  if(newTaks && newTaks.length > 5){
+    this.tasks.update((tasks) => [...tasks, newTaks]);
+  }
+}
 
-
-
-
-
+deleteTaks(index: number){
+  console.log(index);
+  this.tasks.update((taks) => taks.filter((taks, position) => position !== index));
+}
 }
