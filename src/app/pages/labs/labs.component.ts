c0c49a4728312c1abe1f39c68da458c807218850
Andres Norato <carnorat@gmail.com>
2024-01-10 11:28:25 -0500

    binding de angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -7,10 +7,36 @@ import { Component } from '@angular/core';
 })
 export class LabsComponent {
   welcome = 'Hola';
-
   tasks = [
     'Instalar angular',
     'Crear proyecto',
     'Mostrar lista'
   ];
+
+  name = 'Andres';
+  age = 22;
+  status = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  person = {
+    name: 'Carlos',
+    age: 48,
+    img: 'https://w3schools.com/howto/img_avatar.png'
+  }
+
+  clickHandler(){
+    alert('Hola');
+  }
+
+  changeHandler(event: Event){
+    const input =  event.target as HTMLInputElement;
+    const value = input.value
+    console.log(value);
+  }
+
+  keyDownHandler(event: KeyboardEvent){
+    const input =  event.target as HTMLInputElement;
+    const value =  input.value;
+    console.log(value);
+  }
 }
